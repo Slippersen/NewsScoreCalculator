c7210e54bc8311dcbd0aa92be1ca086a62fd8807
Erik J. Sandal <erik.sandal@live.no>
2025-03-08 20:18:00 +0100

    Add unit test project

---
+++ Api.Tests/NewsCalculatorTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Tests;
+
+public class NewsCalculatorTest
+{
+    [Fact]
+    public void CalculateNews_GivenStaticMeasurements()
+    {
+        throw new NotImplementedException();
+    }
+}
