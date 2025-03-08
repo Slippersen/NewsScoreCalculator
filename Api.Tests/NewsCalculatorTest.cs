e4ec63f79ebf7ea4469b53348cb66757ff631e48
Erik J. Sandal <erik.sandal@live.no>
2025-03-08 22:28:52 +0100

    Add unit test for wanted results

--- Api.Tests/NewsCalculatorTest.cs
+++ Api.Tests/NewsCalculatorTest.cs
@@ -1,10 +1,51 @@
-﻿namespace Api.Tests;
+﻿using System.Net.Http.Json;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
 
-public class NewsCalculatorTest
+namespace Api.Tests;
+
+public class NewsCalculatorTest()
 {
     [Fact]
-    public void CalculateNews_GivenStaticMeasurements()
+    public async Task CalculateNews_GivenStaticMeasurements()
     {
-        throw new NotImplementedException();
+        // API specification - POST /news/score
+        // Request body: 
+        //  {
+        //      "measurements": [
+        //          {"type":"TEMP","value":39},
+        //          {"type":"HR","value":43},
+        //          {"type":"RR","value":19}
+        //      ]
+        //  }
+        // Response body: 
+        //  { "score": 2 }
+
+        // Arrange
+        var measurements = new NewsCalculationRequest()
+        {
+            Measurements =
+            [
+                new Measurement(MeasurementType.Temp, 39),
+                new Measurement(MeasurementType.Hr, 43),
+                new Measurement(MeasurementType.Rr, 19)
+            ]
+        };
+
+        var mockNewsCalculatorService = new Mock<INewsCalculatorService>();
+        mockNewsCalculatorService.Setup(service =>
+            service.GetScore(It.IsAny<Measurement[]>())).Returns(new News(2));
+
+        // Act
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+        var response = await client.PostAsJsonAsync("/news/score", measurements);
+
+        // Assert
+        var result = await response.Content.ReadFromJsonAsync<News>();
+        Assert.IsType<News>(result);
+        Assert.Equal(2, result.Score);
     }
 }
---
+++ Api/Models/NewsCalculationRequest.cs
@@ -0,0 +1,3 @@
+namespace Api.Models;
+
+public record struct NewsCalculationRequest(Measurement[] Measurements);
