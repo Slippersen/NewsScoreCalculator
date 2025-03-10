6d190b0f1000be980a03be366b686aec92fb72c0
Erik J. Sandal <erik.sandal@live.no>
2025-03-10 12:03:32 +0100

    Add CORS policy

--- Api/Program.cs
+++ Api/Program.cs
@@ -15,17 +15,36 @@ builder.Services.AddSwaggerGen();
 // NEWS calculator service
 builder.Services.AddScoped<INewsCalculatorService, NewsCalculatorService>();
 
+// CORS policy
+var NewsCalculatorSpecificOrigins = "NewsCalculatorSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: NewsCalculatorSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:3000",
+                "http://localhost:5013",
+                "https://localhost:7144");
+            policy.WithMethods("POST", "OPTIONS");
+            policy.AllowAnyHeader();
+        });
+});
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI();
     app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(NewsCalculatorSpecificOrigins);
 
 app.MapPost("/news/score",
     Results<Ok<News>, BadRequest, InternalServerError>
@@ -41,7 +60,8 @@ app.MapPost("/news/score",
     .WithName("CalculateNews")
     .WithDescription("National Early Warning Score (NEWS)")
     .WithSummary("Calculate the National Early Warning Score (NEWS) based on the measurements provided.")
-    .AddEndpointFilter<NewsCalculationEndpointFilter>();
+    .AddEndpointFilter<NewsCalculationEndpointFilter>()
+    .RequireCors(NewsCalculatorSpecificOrigins);
 
 app.Run();
 
